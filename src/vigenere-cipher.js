741cf0c725656dcebc478ccad7c93d81be8a6b93
LiquidVacuum <venomandroid@gmail.com>
2021-03-30 02:29:22 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
+let letterMap = new Map();
+const firstLetterCode = "A".charCodeAt(0);
+const lastLetterCode = "Z".charCodeAt(0);
+for(let i = firstLetterCode; i <= lastLetterCode; i++) {
+  letterMap.set(String.fromCharCode(i), i - firstLetterCode);
+}
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(directMode) {
+    this.reverseMode = directMode === false;
+  }
+  
+  encrypt(message, key) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let keyChar = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (letterMap.has(message[i])) {
+        if (keyChar === key.length) keyChar = 0;
+        result += String.fromCharCode((letterMap.get(message[i]) + letterMap.get(key[keyChar])) % 26 + firstLetterCode);
+        keyChar++;
+      } else {
+        result += message[i];
+      }
+    }
+    return (this.reverseMode) ? result.split('').reverse().join('') : result;
+  }
+
+  decrypt(message, key) {
+    key = key.toUpperCase();
+
+    let result = '';
+    let keyChar = 0;
+    for (let i = 0; i < message.length; i++) {
+      let char = message[i];
+
+      if (letterMap.has(char)) {
+        if (keyChar === key.length) keyChar = 0;
+        result += String.fromCharCode((letterMap.get(char) - letterMap.get(key[keyChar]) + 26) % 26 + firstLetterCode);
+        keyChar++;
+      } else {
+        result += char;
+      }
+    }
+    return (this.reverseMode) ? result.split('').reverse().join('') : result;
   }
 }
 
