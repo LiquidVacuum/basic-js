72b37b89b20b20c643aff2b214cf2e567ed3bfa1
LiquidVacuum <venomandroid@gmail.com>
2021-03-29 13:03:18 +0300

    feat: add extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  let {repeatTimes : repeatTimes = 1,
+       separator : separator = '+',
+       addition : addition = '',
+       additionRepeatTimes : additionRepeatTimes = 1,
+       additionSeparator : additionSeparator = '|',
+  } = options;
+  let output = [];
+  let additionArray = [];
+  let stringArray = [];
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    additionArray.push(addition + '');
+  }
   
+  stringArray.push(str + '');
+  stringArray.push(additionArray.join(additionSeparator));
+
+  for (let i = 0; i < repeatTimes; i++) {
+    output.push(stringArray.join(''));
+  }
+  
+  return output.join(separator);
+};
